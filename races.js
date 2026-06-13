7dc8c2a4cb03fbb2e787e3edf846c57f160574da
HatMan-Jar <richard.hann0125@gmail.com>
2026-06-13 16:14:59 +0100

    Update races.js

--- races.js
+++ races.js
@@ -1,3 +1,24 @@
 // File Hat Man told me to create
 
-const todaysRaces = [];
+const todaysRaces = [
+  {
+    course: "Haydock",
+    time: "14:30",
+    type: "Handicap",
+    runners: 11,
+    placesPaid: 4,
+    horses: [
+      {
+        name: "Champagne Prince",
+        odds: "8/1",
+        features: [
+          "market_move",
+          "ground_suitability",
+          "better_than_result",
+          "handicap_mark",
+          "trainer_form"
+        ]
+      }
+    ]
+  }
+];
