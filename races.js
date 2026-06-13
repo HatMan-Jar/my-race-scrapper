c1652f3fd5de91f4a867c3c67dcf32536a1a7fdd
HatMan-Jar <richard.hann0125@gmail.com>
2026-06-13 16:07:17 +0100

    Update races.js

--- races.js
+++ races.js
@@ -1,12 +1,3 @@
 // File Hat Man told me to create
 
-const todaysRaces = [
-  {
-    course: "Haydock",
-    time: "14:30",
-    type: "Handicap",
-    runners: 11,
-    placesPaid: 4,
-    horses: []
-  }
-];
+const todaysRaces = [];
